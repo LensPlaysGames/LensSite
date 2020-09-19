ff2531abe723857cfb3fc873dea15f3ea4c1a19a
mrtinytoes <abrahamkelloggson@gmail.com>
2020-09-18 17:07:22 -0700

    V0.0.2.2   --   http -> https

--- JS/index.js
+++ JS/index.js
@@ -14,15 +14,14 @@
         var imgcontainer = $("#randomimg");
         var aRandomNum = Math.floor((Math.random() * 25) + 1);
 
-        $.getJSON('http://www.reddit.com/r/' + subreddit + '.json?jsonp=?&show=all&limit=25', function (data) {
+        $.getJSON('https://www.reddit.com/r/' + subreddit + '.json?jsonp=?&show=all&limit=25', function (data) {
             $.each(data.data.children, function (i, item) {
-
                 if (i == aRandomNum) {
                     if (item.data.post_hint == "image") {
-                        console.log(item.data);
                         imgcontainer.html($("<img>", { src: item.data.url, class: 'imgkeepaspect' }));
                         return false;
-                    } else {
+                    }
+                    else {
                         GetRandRedditImage(subreddit);
                     }
                 }
